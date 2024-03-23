9de13b8879a62b57b44836af4f9590009cdbfa5a
Sozutova Olga <sozutova@list.ru>
2024-03-23 22:56:49 +0700

    Start

---
+++ UnitTestTOPO3/TConvertorUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOPO3.Core;
+
+namespace UnitTestTOPO3
+{
+    [TestClass]
+    public class TConvertorUnitTests
+    {
+        [TestMethod]
+        public void ConvertorClassCreationTest()
+        {
+        }
+
+
+    }
+}
