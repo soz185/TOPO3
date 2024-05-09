f3dc641c248a5bee4089bde41a329335d1796625
Sozutova Olga <sozutova@list.ru>
2024-05-10 02:04:48 +0700

    Added test for LengthFromSIToRussian method. In method 1 metre converts arshin

---
+++ UnitTestTOPO3/LengthFromSIToRussianTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TOPO3.Core;
+
+namespace UnitTestTOPO3
+{
+    [TestClass]
+    class LengthFromSIToRussianTests
+    {
+        [TestMethod]
+        public void From1MetreToArshinTest()
+        {
+            double expected = LengthFromSIToRussian(1, LengthSI.Metre, LengthRussian.Arshin);
+            Assert.AreEqual(expected, 0.7112);
+        }
+    }
+}
